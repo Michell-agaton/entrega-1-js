b25abef6393e7d3b7f747ab05dc5918121219987
Michell-agaton <michell.sanchez.agaton@gmail.com>
2025-08-09 08:02:45 -0600

    calculadora

---
+++ script.js
@@ -0,0 +1,26 @@
+const pantalla = document.querySelector(".pantalla");
+const botones = document.querySelectorAll(".boton");
+
+botones.forEach(boton=>{
+    boton.addEventListener("click",() =>{
+        const botonApretado = boton.textContent;
+
+        if(boton.id === "c"){
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "borrar"){
+            pantalla.textContent = pantalla.textContent.slice(0, -1);
+            return;
+        }else{
+            
+        }
+
+        if(pantalla.textContent === "0"){
+            pantalla.textContent = botonApretado;
+        }else{
+            pantalla.textContent += botonApretado;
+        }
+    })
+})
